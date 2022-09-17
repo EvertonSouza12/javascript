999d619f6db1125cbda01dcd438e589863ef9736
Everton <souza1287@live.com>
2022-09-17 13:10:06 -0300

    parametros- argumentos

---
+++ functions/functions.js
@@ -0,0 +1,15 @@
+// cria aplicativo de frases motivacionais
+
+// declatration - declaração da função
+// function statement
+function createPhrases() {
+  console.log(`Estudar é muito bom`)
+  console.log(`Paciência e persistencia`)
+  console.log(`Revisão é a mãe do aprendizado`)
+}
+
+// executar a função
+// chamar a função
+// execute, run, call, invoke
+
+createPhrases()
