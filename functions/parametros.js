1be36c3c606a4ddd19bd0b334ba11d38c06a3369
Everton <souza1287@live.com>
2022-09-17 13:32:57 -0300

    função

--- functions/parametros.js
+++ functions/parametros.js
@@ -2,8 +2,19 @@
 // function anonymous
 
 //parametros da função
+
+
 const sum = function(number1, number2){
-  console.log(number1 + number2)
+  let total = number1 + number2
+  return total
 }
 
+const soma = sum(2, 5)
+console.log(soma)
+
+
 sum(2, 3) // argumentos
+
+let number1 = 5
+let number2 = 3
+console.log(`a soma é ${sum(number1, number2)}`)
