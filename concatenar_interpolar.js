1a00d0c992e70a790ebc5496a8bf4a06fec036e8
Everton <souza1287@live.com>
2022-09-17 12:10:15 -0300

    concatenar

---
+++ concatenar_interpolar.js
@@ -0,0 +1,20 @@
+// declaração
+var name
+
+// atribuição
+name = "Everton"
+
+// tipo de dado atribuido
+console.log(typeof name)
+
+// agrupamento de declaraçõee
+let age, isHuman
+ 
+age = 18
+isHuman = true
+
+// esrita de texto + variaveis
+console.log(`Eu ` + name + ` tem ` + age + ` anos`)
+
+// interpolando valores
+console.log(`o ${name} tem ${age} anos`)
