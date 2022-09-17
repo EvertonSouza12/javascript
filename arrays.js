aeba8f4fb72c791d69d99ea50fdc170d0230bdf9
Everton <souza1287@live.com>
2022-09-17 12:50:40 -0300

    exercicios

---
+++ arrays.js
@@ -0,0 +1,13 @@
+const animals = [
+  `Lion`,
+  `Monkey`,
+  `cat`
+]
+
+// acessar valores na array
+
+console.log(animals)
+
+console.log(animals[0])
+
+console.log(animals.length)
---
+++ exercicios.js
@@ -0,0 +1,45 @@
+// 1. Declare uma variavel de nome weight
+
+let weight
+
+// 2. Que tipo de dado é a variavel acima
+console.log(typeof weight)
+
+// 3. Declare uma variavel e atribuia valores para cada um dos dados
+
+let name = "Everton"
+let age = 24
+stars = 2.4
+isSubscribed = true
+
+let student = {
+  name: "Everton",
+  age: 24,
+  stars: 96.5,
+  isSubscribed: true
+}
+
+const Jhon = {
+  name: "Jhon",
+  age: 30,
+  stars: 99.9,
+  isSubscribed: false
+}
+
+console.log(student)
+
+console.log(`${student.name} de ${student.age} anos pesa ${student.stars} kg`)
+
+students = []
+
+students = [
+  student,
+  Jhon
+]
+
+console.log(students)
+console.log(students[0])
+console.log(students[1])
+
+console.log(a)
+var a = 1
