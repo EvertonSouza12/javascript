a8f67d7131c36e75c17e150e92844284940ac6fa
Everton <souza1287@live.com>
2022-09-17 12:01:29 -0300

    argumentos

--- variaveis.js
+++ variaveis.js
@@ -5,3 +5,21 @@
 const clima = "Quente"
 
 console.log(clima)
+
+
+// declaração
+var name
+
+// atribuição
+name = "Everton"
+
+// tipo de dado atribuido
+console.log(typeof name)
+
+// agrupamento de declaraçõee
+let age, isHuman
+ 
+age = 18
+isHuman = true
+
+console.log(name, age, isHuman)
