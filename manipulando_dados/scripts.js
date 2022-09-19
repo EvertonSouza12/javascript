b422f92812b226071b88fbfdf4494b7cb04d3313
Everton <souza1287@live.com>
2022-09-19 19:52:56 -0300

    transformar numero quebrado

--- manipulando_dados/scripts.js
+++ manipulando_dados/scripts.js
@@ -1,7 +1,15 @@
 // string em numeros
-let string = `123`
-console.log(Number(string))
-let number = 321
-console.log(String(number))
+// let string = `123`
+// console.log(Number(string))
+//let number = 321
+//console.log(String(number))
 
 // contando caracteres e digitos
+// let word = "Paralelepipedo"
+// console.log(word.length)
+// let number = 1234
+// console.log(String(number).length)
+
+// transformar em numero quebrado
+// let number = 41231231234.412412
+// console.log(Number(number).toFixed(2).replace(".",","))
