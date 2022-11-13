6d74fcf6eea7014e665e7f88a9c4bae5ac1930d1
Everton Souza <108016213+EvertonSouza12@users.noreply.github.com>
2022-11-13 11:32:11 -0300

    Add files via upload

---
+++ jogo_adivinhacao/main.js
@@ -0,0 +1,61 @@
+// Varáveis
+const screen1 = document.querySelector(".screen1")
+const screen2 =  document.querySelector(".screen2")
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
+let randomNumber = Math.round(Math.random() * 10)
+let xAttempts = 1;
+
+// Eventos
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', handleResetClick)
+document.addEventListener('keypress', handleKeyPress)
+
+// Funções
+function validateNumber(){
+  if((Number(inputNumber.value)) > 10){
+    alert("Número invalido")
+  }
+}
+
+function handleTryClick(event){
+  event.preventDefault()
+  const inputNumber = document.querySelector("#inputNumber")
+  validateNumber()
+
+  if(Number(inputNumber.value) == randomNumber){
+    toggleScreen()
+    screen2.querySelector(".screen2 h2").innerText = `Acertou em ${xAttempts} tentativas`
+  }
+  inputNumber.value = ""
+  xAttempts++;
+}
+
+function handleKeyPress(){
+  toggleScreen()
+}
+
+function handleResetClick(){
+  toggleScreen()
+  xAttempts = 1
+  randomNumber
+}
+
+function toggleScreen(){
+  screen1.classList.toggle("hide")
+  screen2.classList.toggle("hide")
+}
+
+function handleKeyPress(){
+  if(e.key == "Enter" && screen1.classList.contains('hide')){
+    handleResetClick()
+  }
+}
+
+function handleKeyPress (e) {
+  if (e.key == 'Enter' && screen1.classList.contains('hide')) {
+    handleResetClick()
+    xAttempts = 1
+    randomNumber
+  }
+}
